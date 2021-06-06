f6cde5d369af38d46c33a87d8b856af356c21a9e
Ibrahim Rehman <ibrahimrehman0346@gmail.com>
2021-06-06 17:03:25 +0500

    make it better

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv")
 const getCustomers = require("./controllers/getCustomers");
 const postTransfers = require("./controllers/postTransfer");
 const putCustomers = require("./controllers/putCustomers");
+const path = require("path");
 
 dotenv.config()
 
@@ -19,5 +20,14 @@ app.use(express.json())
 app.get("/customers", getCustomers);
 app.post("/transfers", postTransfers);
 app.put("/updatecustomers", putCustomers)
+
+
+if (process.env.NODE_ENV === "production"){
+
+    app.use(express.static("banking-website/build"))
+    app.get("*", (req, res)=>{
+        res.sendFile(path.resolve(__dirname, "banking-website", "build", "index.html"))
+    })
+}
 
 app.listen(PORT, ()=>console.log(`Server running on PORT ${PORT}`))
